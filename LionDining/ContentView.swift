93b9ba4586fbb15f44a42a41017103a9b72f3006
Catherine Zhao <zhiruoz2@illinois.edu>
2024-06-15 22:29:43 -0400

    change background style and image round rectangular

--- LionDining/ContentView.swift
+++ LionDining/ContentView.swift
@@ -13,14 +13,14 @@ import SwiftUI
 struct ContentView: View {
     
     let diningHalls = [
-            DiningHalls(name: "John Jay Dining Hall", photo: "john-jay-cover", hours: "Closed for next 1 week"),
+            DiningHalls(name: "John Jay Dining", photo: "john-jay-cover", hours: "Closed for next 1 week"),
             DiningHalls(name: "JJ's Place", photo: "jj-place-cover", hours: "Closed for next 1 week"),
             DiningHalls(name: "Faculty House", photo: "faculty-house-cover", hours: "Closed for next 1 week"),
             DiningHalls(name: "Ferris Booth Commons", photo: "ferris-booth-commons-cover", hours: "Closed for next 1 week"),
             DiningHalls(name: "Chef Mike's Sub Shop", photo: "chef-mike-sub-shop-cover", hours: "Closed for next 1 week"),
             DiningHalls(name: "Chef Don's Pizza Pi", photo: "chef-don-pizza-pi-cover", hours: "Closed for next 1 week"),
-            DiningHalls(name: "Grace Dodge Dining Hall", photo: "grace-dodge-cover", hours: "Closed for next 1 week"),
-            DiningHalls(name: "Robert F. Smith Dining Hall", photo: "robert-f-smith-cover", hours: "Closed for next 1 week"),
+            DiningHalls(name: "Grace Dodge Dining", photo: "grace-dodge-cover", hours: "Closed for next 1 week"),
+            DiningHalls(name: "Robert F. Smith Dining", photo: "robert-f-smith-cover", hours: "Closed for next 1 week"),
             DiningHalls(name: "The Fac Shack", photo: "fac-shack-cover", hours: "Closed for next 1 week")
         ]
     
@@ -35,6 +35,7 @@ struct ContentView: View {
                     Text(diningHalls.name)
                 }
         }
+        .listStyle(PlainListStyle())
     }
 }
 
@@ -49,22 +50,23 @@ struct DiningHallCellView: View {
             Image(diningHall.photo)
                 .resizable()
                 .aspectRatio(contentMode: .fit)
-                .frame(width: 100)
+                .frame(width: 80)
                 .clipped()
+                .clipShape(RoundedRectangle(cornerRadius: 10))
                 .padding(.vertical, 10)
             VStack(alignment: .leading){
                 Text(diningHall.name)
-                    .font(.system(size: 20))
+                    .font(.system(size: 14))
                     .fontWeight(.bold)
-                    .foregroundColor(Color(hex: "#004aa8"))
                     .padding(.leading, 3)
+                    .padding(.bottom, 5)
                 Text(diningHall.hours)
-                    .font(.subheadline)
+                    .font(.system(size: 12))
                     .foregroundColor(.gray)
                     .padding(.leading, 3)
             }
             .padding(.leading, 5)
-            .padding(.top, 10)
+            .padding(.top, 15)
             .padding(.bottom, 10)
             Spacer()
         }
