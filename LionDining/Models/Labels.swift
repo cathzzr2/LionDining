c079be557fb1988876d3ca71250ae953f968d81d
Catherine Zhao <zhiruoz2@illinois.edu>
2024-06-17 22:05:58 -0400

    recreate lost file Labels.swift

---
+++ LionDining/Models/Labels.swift
@@ -0,0 +1,16 @@
+//
+//  Labels.swift
+//  LionDining
+//
+//  Created by Catherine Zhao on 2024/6/17.
+//
+
+import Foundation
+import SwiftUI
+
+struct Labels: Identifiable, Hashable {
+    let id = UUID()
+    var text: String
+    var color: Color
+}
+
